980cd74a2c740539e4531a4651835c6f7200c5b7
JehanD11 <jehandesai121@gmail.com>
2024-05-04 01:28:12 +0530

    added dummy data

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from maps import googleMapsHandler
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +14,21 @@ def index():
 
 @app.route("/submit")
 def optimus(data):  # TODO : change this
+    data = { 
+        "places" :[
+        "Gateway of India",
+        "Marine Drive",
+        "Chhatrapati Shivaji Maharaj Terminus (CST)",
+        "Siddhivinayak Temple",
+        "Haji Ali Dargah",
+        "Juhu Beach",
+        "Elephanta Caves",
+        "Bandra-Worli Sea Link",
+        "Film City",
+        "Colaba Causeway"
+        ],
+        "time" : time.time()
+    }
     m = googleMapsHandler(data)
 
 
