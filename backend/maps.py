fbe5675fba65b2a0f8b865d2043ffaa015233f2d
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-05-04 23:43:31 +0530

    fetch route matrix from api

--- backend/maps.py
+++ backend/maps.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 import os
 from googlemaps import Client, geocoding
 import json
-import googlemaps
+import requests
 
-# TODO : LLM integration pending
+
 class googleMapsHandler:
     def __init__(self, data):
         load_dotenv()
@@ -14,16 +14,7 @@ class googleMapsHandler:
         self.places = data["places"]
         self.client = Client(key=self.apikey)
 
-    def get_directions(self):
-        """
-        takes the places mentioned in the data and returns the optimised route using directions api.
-        """
-        gmaps = googlemaps.Client(key = self.apikey)
-        distance_matrix = gmaps.distance_matrix([place['places']['coordinates'] for place in self.data],
-                                                [place['location']['coordinate'] for place in self.data])
-        distances = [[row['distance']['value'] for row in matrix['rows']] for matrix in distance_matrix['rows']]
-        
-    def geocode(self):
+    def geocode(self, debug=False):
         """
         returns the lat long for the given list of locations
         """
@@ -32,30 +23,39 @@ class googleMapsHandler:
             gc = geocoding.geocode(self.client, address=i)
             loc_geocode_map[i] = gc[0]["geometry"]
 
-        # return json.dumps(loc_geocode_map, indent=2)
+        if debug:
+            return json.dumps(loc_geocode_map, indent=2)
         return loc_geocode_map
 
-    def gen_payload(self):
+    def gen_payload(self, debug=False):
         geocodes = self.geocode()
         payload = []
         for i in geocodes:
-            bp = {
-                "waypoint": {
-                    "location": {
-                        "latLng": {
-                            "latitude": 0, 
-                            "longitude": 0
-                        }
-                    }
-                }
-            }
+            bp = {"waypoint": {"location": {"latLng": {"latitude": 0, "longitude": 0}}}}
             lat = geocodes[i]["location"]["lat"]
             lng = geocodes[i]["location"]["lng"]
             bp["waypoint"]["location"]["latLng"]["latitude"] = lat
             bp["waypoint"]["location"]["latLng"]["longitude"] = lng
             payload.append(bp)
-        
+
+        if debug:
+            return json.dumps(payload, indent=2)
         return payload
+
+    def get_route_matrix(self, debug=False):
+        endpoint = "https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix"
+        p = self.gen_payload()
+        payload = {"origins": p, "destinations": p}
+        headers = {
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": self.apikey,
+            "X-Goog-FieldMask": "originIndex,destinationIndex,duration,distanceMeters,status,condition,travelAdvisory.transitFare",
+        }
+        resp = requests.post(endpoint, json=payload, headers=headers)
+        route_mat = resp.text
+        if debug:
+            return json.dumps(route_mat, indent=2)
+        return route_mat
 
     def get_places(self):
         """
@@ -69,10 +69,8 @@ class googleMapsHandler:
         """
         ...
 
-
     def aggregate(self):
         """
         returns a dict that has all the information
         """
         return self.data
-
