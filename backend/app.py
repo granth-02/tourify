bd88aa857c2db11a86a9f1dfa65e738189b64281
granth <75667699+granth-02@users.noreply.github.com>
2024-05-04 11:11:07 +0530

    fix bugs

--- backend/app.py
+++ backend/app.py
@@ -30,24 +30,24 @@ def index():
 def optimus():  # TODO : change this
     data = request.get_json()
     data = dict(data)
-    print(f"places {data["places"]}")
-    print(f"time {data["time"]}")
+    print(f"places {data['places']}")
+    print(f"time {data['time']}")
     m = googleMapsHandler(data)
     resp = m.aggregate()  # send response back to the front end
     return resp
 
 
-@app.route('/test', methods=['GET', 'POST'])
-def handle_data():
-    global initial_data  # Ensure we're accessing the global variable
-    if request.method == 'GET':
-        return jsonify(initial_data)
-    elif request.method == 'POST':
-        data = request.json
-        initial_data['places'] = data['places']
-        initial_data['time'] = data['time']
-        print("Data received from React:", data)  # Print the received data for debugging
-        return 'Data updated successfully', 200
+# @app.route('/test', methods=['GET', 'POST'])
+# def handle_data():
+#     global initial_data  # Ensure we're accessing the global variable
+#     if request.method == 'GET':
+#         return jsonify(initial_data)
+#     elif request.method == 'POST':
+#         data = request.json
+#         initial_data['places'] = data['places']
+#         initial_data['time'] = data['time']
+#         print("Data received from React:", data)  # Print the received data for debugging
+#         return 'Data updated successfully', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
